f9bd566fde4bf15288ff7c83e23826d58d8abe29
mattcyprinus <20032744@students.koi.edu.au>
2025-12-05 12:57:52 +1100

    Uploaded files

---
+++ Tutorial6Ex2/Controllers/MyIDController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tutorial6Ex2.Controllers
+{
+    public class MyIDController : Controller
+    {
+        // GET: MyID
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DescribeMyself()
+        {
+            ViewData["Name"] = "Aven Matthew Majellano";
+            ViewData["StudentID"] = "20032744";
+            return View();
+
+        }
+    }
+}
---
+++ Tutorial6Ex2/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tutorial6Ex2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
